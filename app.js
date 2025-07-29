a1e76deff069fa8a5b40cb7e45105181386d6ea3
uzAsco <sulaymonxonovakbarxon@gmail.com>
2025-07-29 13:07:00 +0500

    my codewars codes

---
+++ app.js
@@ -0,0 +1,22 @@
+// def two_oldest_ages(ages):
+//     sorted_ages = sorted(ages)
+//     return [sorted_ages[-2], sorted_ages[-1]]
+
+
+
+// def sum_of_minimums(numbers):
+//     return sum(min(row) for row in numbers)
+
+
+//def uefa_euro_2016(teams, scores):
+    // team1, team2 = teams
+    // score1, score2 = scores
+
+    // if score1 > score2:
+    //     result = f"{team1} won!"
+    // elif score1 < score2:
+    //     result = f"{team2} won!"
+    // else:
+    //     result = "teams played draw."
+
+    // return f"At match {team1} - {team2}, {result}"
