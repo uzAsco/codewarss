3c0879cad48f70aa4202b9b57aace197a94ae86e
uzAsco <sulaymonxonovakbarxon@gmail.com>
2025-07-31 11:09:23 +0500

    codewars codes

--- app.js
+++ app.js
@@ -20,3 +20,52 @@
     //     result = "teams played draw."
 
     // return f"At match {team1} - {team2}, {result}"
+
+
+
+
+  //  29.07.2025 homework
+
+//bu function da array 1 bilan array 2 sort qilib qo'shilyapti 
+//   def merge_arrays(arr1, arr2):
+//     return sorted(set(arr1 + arr2))
+
+
+// bu functionda array ichidagi qiymatlar array methods yordamida o'ng dan chapga, yoki chapdan o'ng ga ko'chirilyapti 
+//function loopArr(arr, direction, steps) {
+    // for (let i = 0; i < steps; i++) {
+    //     if (direction === 'left') {
+    //       arr.push(arr.shift());
+    //     } else if (direction === 'right') {
+    //       arr.unshift(arr.pop());
+    //     }
+    //   }
+    //   return arr;
+    // }
+
+
+// bu function da 2 ta array ichida harflar va raqamlar bor, arraylarni birlashtirib raqamlar bir biriga qo'shilyapti
+    // function combine(...objects) {
+    //     return objects.reduce((acc, obj) => {
+    //       for (let key in obj) {
+    //         acc[key] = (acc[key] || 0) + obj[key];
+    //       }
+    //       return acc;
+    //     }, {});
+    //   }
+      
+
+
+//bu function da includes yordamida array ichida birorta qiymat bormi yoki yoq tekshirilyapti
+    // function check(a, x) {
+    //     return a.includes(x);
+    //   }
+      
+
+
+//bu functionda math.min/max yordamida raqamlardan eng kattasi va eng kichigi aniqlanyapti
+    // function minMax(arr) {
+    //     return [Math.min(...arr), Math.max(...arr)];
+    //   }
+      
+    
